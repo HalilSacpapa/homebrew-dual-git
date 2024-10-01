2e91c51bdf4d8a116f4d5b10ed62fda336d4c721
Halil Sacpapa <halil.bagdadi@epitech.eu>
2024-10-01 14:47:52 +0200

    fix: improve setup_dual_ssh readability and usability

--- setup_dual_ssh.sh
+++ setup_dual_ssh.sh
@@ -2,16 +2,15 @@ echo "This script will create two ssh keys for your github accounts.\nWould you 
 read response
 
 if [ response == "n" ]; then
-    echo "Exiting script"
     exit 1
 fi
 
-echo "Enter the email adress of your first account:"
+echo "\nEnter the email adress of your first account:"
 read email1
 echo "Enter the name of your first account:"
 read name1
 
-echo "Enter the email adress of your second account:"
+echo "\nEnter the email adress of your second account:"
 read email2
 echo "Enter the name of your second account:"
 read name2
@@ -21,36 +20,41 @@ echo "the following ssh keys will be created:\n\t$email1 (~/.ssh/$name1.pub)\n\t
 read response
 
 if [ response == "n" ]; then
-    echo "Exiting script"
     exit 1
 fi
 
-echo "Creating ssh keys...\n"
-eval "$(ssh-keygen -t rsa -b 4096 -C email1 -f name1)"
-eval "$(ssh-keygen -t rsa -b 4096 -C email2 -f name2)"
-echo "\n\nssh keys created successfully."
+echo "\n\n"
+ssh-keygen -t rsa -b 4096 -C "$email1" -f ~/.ssh/$name1
+echo "\n\n"
+ssh-keygen -t rsa -b 4096 -C "$email2" -f ~/.ssh/$name2
+echo "\n\nssh keys successfully created to the ~/.ssh directory.\n\n"
 
 echo "Copying $name1 ssh keys to clipboard...\n"
-eval "$(pbcopy < ~/.ssh/$name1.pub)"
+$(pbcopy < ~/.ssh/$name1.pub)
 echo "You can now paste the key into your $email1 github account settings.\nhttps://github.com/settings/ssh/new\nConfigure SSO if necessary.\n"
+echo "Press enter to continue...\n\n"
+read cb
 
 echo "Copying $name2 ssh keys to clipboard...\n"
-eval "$(pbcopy < ~/.ssh/$name2.pub)"
+$(pbcopy < ~/.ssh/$name2.pub)
 echo "You can now paste the key into your $email2 github account settings.\nhttps://github.com/settings/ssh/new\nConfigure SSO if necessary.\n"
-
-echo "We will setup the ssh config file located at ~/.ssh/config.\n"
-domain1 = "github.com-$name1"
-domain2 = "github.com-$name2"
-echo "Host $domain1\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/$name1\n\nHost $domain2\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/$name2" >> ~/.ssh/config
+echo "Press enter to continue...\n\n"
+read cb
 
 echo "Which account would you like to use by default?\n\t1. $name1 ($email1)\n\t2. $name2 ($email2)"
 read response
-if [ response == "1" ]; then
-    echo "Using $name1 with $email1 by default."
-    git config --global user.name $name1
-    git config --global user.email $email1
-elif [ response == "2" ]; then
-    echo "Using $name2 with $email2 by default."
-    git config --global user.name $name2
-    git config --global user.email $email2
+
+echo "We will setup the ssh config file located at ~/.ssh/config.\n"
+domain1="github.com-$name1"
+domain2="github.com-$name2"
+echo "Host $domain1\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/$name1\n\nHost $domain2\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/$name2" >> ~/.ssh/config
+
+if [ $response == "1" ]; then
+    echo "\nUsing $name1 with $email1 by default."
+    $(git config --global user.name "$name1")
+    $(git config --global user.email $email1)
+elif [ $response == "2" ]; then
+    echo "\nUsing $name2 with $email2 by default."
+    $(git config --global user.name "$name2")
+    $(git config --global user.email $email2)
 fi
