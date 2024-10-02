d41aed64aed8edfcc05fbc8b24b286327a7253aa
Halil BAGDADI <50623087+HalilSacpapa@users.noreply.github.com>
2024-10-02 18:14:18 +0200

    Update dual-git.rb sha256

--- Formula/dual-git.rb
+++ Formula/dual-git.rb
@@ -2,7 +2,7 @@ class DualGit < Formula
     desc "Script that manages multiple SSH configurations for Git"
     homepage "https://github.com/HalilSacpapa/homebrew-dual-git"
     url "https://raw.githubusercontent.com/HalilSacpapa/homebrew-dual-git/refs/heads/main/dual_git.sh"
-    sha256 "46086d9a5422fd4886d7cbd75fffa0cc59570196db5beaf9a5358c3e395612b3"
+    sha256 "2cd29f83e4cb83c28d2724308ce58ff4068f5537987003a6ea77ca2228f215c6"
 
     version "1.0"
 
